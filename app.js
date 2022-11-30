8e3e30ec838e9b575e35988d322a0b9aadc90c1d
jozkan30 <jozkan2@gmail.com>
2022-11-30 10:43:18 -0500

    html updates and more js

--- app.js
+++ app.js
@@ -1,24 +1,29 @@
+// array of possible choices 
 const choices=[
 1,2,3,4,5
 ]
 
+/// There random events for the three slots of the machine that choose from 5 posible choices 
 let slotA= Math.floor(Math.random()* choices.length)
 let slotB= Math.floor(Math.random()* choices.length)
 let slotC= Math.floor(Math.random()* choices.length)
 
 let result = [slotA,slotB,slotC];
+console.log(result); 
 
 
+/// Outcomes that are posible: 3 match | 2 match | none match 
+function outcomes(){
 if(slotA==slotB && slotA==slotC && slotB==slotC){
-    console.log(`${result} you Won!`)
+    return (`${result} you Won!`)
 } else if(slotA==slotB || slotA==slotC || slotB==slotC){
-    console.log([slotA, slotB, slotC] +  " Hey, Two in a row is good")
+     return [slotA, slotB, slotC] +  " Hey, Two in a row is good"
 } else{
-    console.log(`${result}`);
-}
+     return `${result}`
+}}
 
+// DOM references:
 
-function getElements(){
-document.getElementsByClassName("slotOne").innerHtml;
+const firstOutCome= document.getElementById('1').innerHTML="A new Word";
 
-}
+console.log(firstOutCome);
