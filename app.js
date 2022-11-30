7a5d1dc92dcb1357adf03cf7509648d4567dbd8a
jozkan30 <jozkan2@gmail.com>
2022-11-30 13:45:25 -0500

    commit before images are added to array

--- app.js
+++ app.js
@@ -2,7 +2,8 @@
 const choices=[
 0,1,2,3,4
 ]
-console.log()
+
+
 /// There random events for the three slots of the machine that choose from 5 posible choices 
 let slotA= Math.floor(Math.random()* choices.length)
 let slotB= Math.floor(Math.random()* choices.length)
@@ -11,26 +12,13 @@ let slotC= Math.floor(Math.random()* choices.length)
 let result = [slotA,slotB,slotC];
 console.log(result); 
 
-
-/// Outcomes that are posible: 3 match | 2 match | none match 
-function outComes(){
-if(slotA==slotB && slotA==slotC && slotB==slotC){
-    return (`${result} you Won!`)
-} else if(slotA==slotB || slotA==slotC || slotB==slotC){
-     return [slotA, slotB, slotC] +  " Hey, Two in a row is good"
-} else{
-     return `${result}`
-}}
-outComes();
-
-
-
-// DOM references:
+// DOM references &  outcomes that are posible: 3 match | 2 match | none match 
 function addElements(){
 firstOutCome= document.getElementById('0').innerHTML=slotA;
 secondOutCome= document.getElementById('1').innerHTML=slotB;
 thirdOutCome= document.getElementById('2').innerHTML=slotC;
 
+// outcome possiblities
 if(slotA==slotB && slotA==slotC && slotB==slotC){
     return document.getElementById('result').innerHTML="You Won"
 }else if(slotA==slotB || slotA==slotC || slotB==slotC){
--- extra.js
+++ extra.js
@@ -3,3 +3,15 @@
 // const firstOutCome= document.getElementById('0').innerHTML=slotA
 // const secondOutCome= document.getElementById('1').innerHTML=slotB
 // const thirdOutCome= document.getElementById('2').innerHTML=slotC
+
+
+// function outComes(){
+//     if(slotA==slotB && slotA==slotC && slotB==slotC){
+//         return (`${result} you Won!`)
+//     } else if(slotA==slotB || slotA==slotC || slotB==slotC){
+//          return [slotA, slotB, slotC] +  " Hey, Two in a row is good"
+//     } else{
+//          return `${result}`
+//     }}
+//     outComes();
+    
