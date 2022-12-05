5d1cf1a679fb38829fd98b6a8543d6e0a315c003
jozkan30 <jozkan2@gmail.com>
2022-12-05 11:40:59 -0500

    new html && CSS added

---
+++ maker.js
@@ -0,0 +1,16 @@
+let questions=[
+"What is the largest country in the world based on the land area?",
+"What is the oldest inhabited city in the world?",
+"How many moons does Saturn have?",
+"How many legs does a lobster have?"]
+
+let answers=[
+"Russia",
+"Dammascus",
+"82",
+"10"]
+
+function  getElements(id){
+ return document.getElementById(id)
+
+}
